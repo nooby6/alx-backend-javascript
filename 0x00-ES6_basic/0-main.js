d20578c8ff04fc9b4405c1c1ab5407f1ef23cd67
Nooby6 <giftaudrine1@gmail.com>
2024-10-02 13:55:58 +0000

    0-main.js

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,7 +1,3 @@
 import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 0-main.js 
-I prefer const when I can. But sometimes let is okay
