8e6835de81a19d9f3f3eb9e67beb4f55307f6cd5
Nooby6 <giftaudrine1@gmail.com>
2024-10-05 21:55:19 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-unused-vars */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    // Resolve the promise with some value (or without any value)
+    resolve();
+  });
 }
