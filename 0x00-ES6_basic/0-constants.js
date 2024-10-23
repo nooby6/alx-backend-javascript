2ab14bfd2243561c220b669182fa1bc1c83831d6
nooby6 <giftaudrine1@gmail.com>
2024-10-23 17:48:56 +0000

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,3 +1,6 @@
+//unction taskFirst to instantiate variables using const
+//function taskNext to instantiate variables using let
+
 export function taskFirst() {
   var task = 'I prefer const when I can.';
   return task;
