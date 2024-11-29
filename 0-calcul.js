32e1e63d5944172ea1350bbc0775214198146e65
nooby6 <giftaudrine1@gmail.com>
2024-11-29 07:46:54 +0300

    Add calculateNumber function to round and sum two numbers

---
+++ 0-calcul.js
@@ -0,0 +1,13 @@
+/**
+ * Rounds two numbers and returns their sum.
+ *
+ * @param {number} a - The first number to be rounded and added.
+ * @param {number} b - The second number to be rounded and added.
+ * @returns {number} The sum of the rounded numbers.
+ */
+function calculateNumber(a, b) {
+    return Math.round(a) + Math.round(b);
+  }
+  
+  module.exports = calculateNumber;
+  
