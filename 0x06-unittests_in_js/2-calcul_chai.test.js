ebf8daea94328b03088bbfe8cab8bf9c9806269c
nooby6 <giftaudrine1@gmail.com>
2024-11-29 08:17:50 +0300

    Refactor unit tests for calculateNumber function to use consistent formatting and improve readability

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,5 @@
-import { expect } from 'chai';
+import chai from 'chai';
+const { expect } = chai;
 import calculateNumber from './2-calcul_chai.js';
 
 // Test suite for the calculateNumber function
